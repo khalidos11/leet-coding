4cb2b37a0315790fc683786a0f889dbb492b68d7
Rithikha Kalidos <rithu.rithikha@gmail.com>
2020-05-16 16:30:18 +0530

    Create 328-oddevenlinkedlist.c

---
+++ 328-oddevenlinkedlist.c
@@ -0,0 +1,34 @@
+/* 
+  Given a singly linked list, group all odd nodes together followed by the even nodes. Please note here we are talking about the node number and not the value in the nodes.
+
+  You should try to do it in place. The program should run in O(1) space complexity and O(nodes) time complexity.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* oddEvenList(struct ListNode* head){
+    if (head == NULL || head->next == NULL) return head;
+    struct ListNode *traversal = head->next->next, *lastOdd = head, *lastEven = head->next, *firstEven = lastEven;
+    while (traversal != NULL)
+    {
+        lastOdd->next = traversal;
+        lastOdd = traversal;
+        if (traversal->next != NULL)
+        {
+            lastEven->next = traversal->next;
+            lastEven = traversal->next;
+        }
+        else break;
+        traversal = traversal->next->next;
+    }
+    lastOdd->next = firstEven;
+    lastEven->next = NULL;
+    return head;
+}
