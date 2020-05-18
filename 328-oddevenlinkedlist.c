1d56fd4f56776ada06af5c0d287f5ee4613fa9b9
Rithikha Kalidos <rithu.rithikha@gmail.com>
2020-05-19 01:06:36 +0530

    Update 328-oddevenlinkedlist.c

--- 328-oddevenlinkedlist.c
+++ 328-oddevenlinkedlist.c
@@ -2,6 +2,10 @@
   Given a singly linked list, group all odd nodes together followed by the even nodes. Please note here we are talking about the node number and not the value in the nodes.
 
   You should try to do it in place. The program should run in O(1) space complexity and O(nodes) time complexity.
+  
+  Runtime: 4ms
+  Memory: 6.7MB
+  Top 94.53% of C submissions
 */
 
 /**
