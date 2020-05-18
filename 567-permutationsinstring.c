745ae8f23ce5664d36b4b09d89e737b37d2e511d
Rithikha Kalidos <rithu.rithikha@gmail.com>
2020-05-19 01:09:13 +0530

    Update 567-permutationsinstring.c

--- 567-permutationsinstring.c
+++ 567-permutationsinstring.c
@@ -1,5 +1,9 @@
 /*
   Given two strings s1 and s2, write a function to return true if s2 contains the permutation of s1. In other words, one of the first       string's permutations is the substring of the second string.
+  
+  Runtime: 0ms
+  Memory: 5.6MB
+  Top 100% of C submissions
 */
 
 #include <string.h>
