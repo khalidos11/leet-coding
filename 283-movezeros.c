b820b07e36f1c3ac12630401d13b10b7d94e2f19
Rithikha Kalidos <rithu.rithikha@gmail.com>
2020-05-19 00:58:40 +0530

    Update 283-movezeros.c

--- 283-movezeros.c
+++ 283-movezeros.c
@@ -1,5 +1,9 @@
 /*
   Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+  
+  Runtime: 12ms
+  Memory: 7.2MB
+  Top 85.65% of C submissions
 */
 
 void moveZeroes(int* nums, int numsSize){
