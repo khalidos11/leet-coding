68d15c9e45cc8c10dd1d8ae9f322414db73c55dc
Rithikha Kalidos <rithu.rithikha@gmail.com>
2020-05-19 00:57:39 +0530

    Update 540-singleelementinsortedarray.c

--- 540-singleelementinsortedarray.c
+++ 540-singleelementinsortedarray.c
@@ -1,5 +1,9 @@
 /*
   You are given a sorted array consisting of only integers where every element appears exactly twice, except for one element which appears    exactly once. Find this single element that appears only once.
+  
+  Runtime: 8ms
+  Memory: 6.3MB
+  Top 75.66% of C submissions
 */
 
 int singleNonDuplicate(int* nums, int numsSize){  
